997b6330fcddc6ecb528e7909b928b1516babae0
funtari76 <funtari777@gmail.com>
2022-10-25 09:34:09 +0000

    profile model migration

--- profiles/models.py
+++ profiles/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 
 class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=95, blank=True)
     content = models.TextField(blank=True)
-    image = models.ImageField(upload_to='images/', default='../default_profile_qdjgyp')  # qura
+    image = models.ImageField(upload_to='images/', default='../default_profile_qdjgyp')   # qora
     update_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
